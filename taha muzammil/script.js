f10804f078be93d854393e6cad12318cfe0831f0
TAHAISMAYA <112887541+TAHAISMAYA@users.noreply.github.com>
2022-11-23 20:58:31 +0500

    Add files via upload

---
+++ taha muzammil/script.js
@@ -0,0 +1,78 @@
+
+// let x=5;
+// switch (x==5){
+
+//     case (x=5):
+//         console.log("x=5");
+//     break;
+
+//     case (x<5):
+//         console.log("x>5");
+//     break;
+
+//     case (x>5):
+//         console.log("x<5");
+//     break;
+
+//     default:
+//         console.log("ok");
+//     break;}
+
+
+// let y= parseInt(prompt("Enter Number or Name of the Month"));
+// switch(y){
+
+// case 1|| "january":
+// console.log("this month is january");
+// break;
+// case 2|| "feburary":
+// console.log("this month is feburary");
+// break;
+// case 3|| "march":
+// console.log("this month is march");
+// break;
+
+// case 4|| "april":
+// console.log("this month is april");
+// break;
+// case 5|| "may":
+// console.log("this month is may");
+// break;
+// case 6|| "june":
+// console.log("this month is june");
+// break;
+// case 7|| "july":
+// console.log("this month is july");
+// break;
+// case 8|| "august":
+// console.log("this month is august");
+// break;
+// case 9|| "september":
+// console.log("this month is september");
+// break;
+// case 10|| "october":
+// console.log("this month is october");
+// break;
+// case 11|| "november":
+// console.log("this month is november");
+// break;
+// case 12|| "december":
+// console.log("this month is december");
+// break;
+// default:
+// console.log("Invalid Month");
+
+// break;
+// }
+
+
+
+
+
+// for(var x=0; x<=5; x++){
+//     console.log(x);
+
+// }
+for(var y=1; y<=20;y++){
+    document.write("2 X " + y + "=" + y*2 +"<br>")
+}
